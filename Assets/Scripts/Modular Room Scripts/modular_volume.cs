23606782ee963d37715cfced66be76b98a571ea9
pclank <31415965+pclank@users.noreply.github.com>
2021-05-29 21:40:23 +0300

    Added Basic Function to Change Color Value.

--- Assets/Scripts/Modular Room Scripts/modular_volume.cs
+++ Assets/Scripts/Modular Room Scripts/modular_volume.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering;
 
 public class modular_volume : MonoBehaviour
 {
@@ -8,13 +10,16 @@ public class modular_volume : MonoBehaviour
     // Public Variables
     // ************************************************************************************
 
-
+    public float[] anger_color = new float[3];
+    public float[] happiness_color = new float[3];
+    public float[] sadness_color = new float[3];
 
     // ************************************************************************************
     // Private Variables
     // ************************************************************************************
 
     private bool player_trigger = false;    // Player In/Out of Volume
+    private ColorAdjustments color_adjust;  // Color Adjustment Object
 
     // ************************************************************************************
     // Trigger Functions
@@ -35,6 +40,23 @@ public class modular_volume : MonoBehaviour
             player_trigger = false;
         }
     }
+
+    // ************************************************************************************
+    // Member Functions
+    // ************************************************************************************
+
+    // Change Color
+
+    private void changeColor(float[] colors)
+    {
+        Color new_color = new Color(colors[0] / 255.0f, colors[1] / 255.0f, colors[2] / 255.0f, 1.0f);
+
+        //var color_parameter = new ColorParameter(new_color);
+
+        color_adjust.colorFilter.Override(new_color);
+    }
+
+    // TODO: Add Function to Calculate Smooth Change to Color.
 
     // Start is called before the first frame update
     void Start()
