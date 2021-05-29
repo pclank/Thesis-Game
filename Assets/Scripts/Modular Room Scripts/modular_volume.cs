75feaafb9e37996fbf7d862c02b04fef71dd6925
pclank <31415965+pclank@users.noreply.github.com>
2021-05-29 22:19:22 +0300

    Finished Color Changing Function and Getting ColorAdjustments Component.

--- Assets/Scripts/Modular Room Scripts/modular_volume.cs
+++ Assets/Scripts/Modular Room Scripts/modular_volume.cs
@@ -19,6 +19,8 @@ public class modular_volume : MonoBehaviour
     // ************************************************************************************
 
     private bool player_trigger = false;    // Player In/Out of Volume
+
+    private Volume volume;                  // Volume Component
     private ColorAdjustments color_adjust;  // Color Adjustment Object
 
     // ************************************************************************************
@@ -49,11 +51,11 @@ public class modular_volume : MonoBehaviour
 
     private void changeColor(float[] colors)
     {
-        Color new_color = new Color(colors[0] / 255.0f, colors[1] / 255.0f, colors[2] / 255.0f, 1.0f);
+        Color new_color = new Color(colors[0] / 255.0f, colors[1] / 255.0f, colors[2] / 255.0f, 1.0f);  // Build Color
 
         //var color_parameter = new ColorParameter(new_color);
 
-        color_adjust.colorFilter.Override(new_color);
+        color_adjust.colorFilter.Override(new_color);                                                   // Apply Color to Filter
     }
 
     // TODO: Add Function to Calculate Smooth Change to Color.
@@ -61,12 +63,20 @@ public class modular_volume : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Get Volume Component
+        volume = GetComponent<Volume>();
+
+        // Get Color Adjustments Component
         
+        if (volume.profile.TryGet(out color_adjust))
+        {
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        changeColor(sadness_color);
     }
 }
