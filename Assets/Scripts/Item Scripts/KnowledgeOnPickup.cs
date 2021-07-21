0c920987d6dfa45ad0ace9e124efb3e41b866306
pclank <31415965+pclank@users.noreply.github.com>
2021-07-21 21:25:50 +0300

    Added First Item Script for Knowledge Updating.

---
+++ Assets/Scripts/Item Scripts/KnowledgeOnPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public int target_item_id = 0;
+    public int target_knowledge_level = 0;
+
+    // Message from Inventory that Item was Picked Up, and Increase Knowledge
+
+    public void increaseKnowledge()
+    {
+        // Make Sure Editor Values are Valid
+        if (target_item_id > 0 && target_knowledge_level > 0)
+        {
+            GameObject.FindWithTag("Player").GetComponent<main_inventory>().increaseKnowledge(target_item_id, target_knowledge_level);
+        }
+    }        
+}
