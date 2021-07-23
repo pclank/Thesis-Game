ee66150ab3b42b484454abfaf05179ab0701ec5a
pclank <31415965+pclank@users.noreply.github.com>
2021-07-23 20:06:52 +0300

    Change of Clip Based on Emotion Implemented.

--- Assets/Scripts/Modular Room Scripts/DynamicMusic.cs
+++ Assets/Scripts/Modular Room Scripts/DynamicMusic.cs
@@ -5,12 +5,16 @@ using System.Collections;
 // Script for Dynamic Music Based on Emotion
 // ************************************************************************************
 
+[RequireComponent(typeof(AudioSource))]
 public class DynamicMusic : MonoBehaviour
 {
     // ************************************************************************************
     // Public Variables
     // ************************************************************************************
 
+    public AudioSource main_source;
+
+    public AudioClip default_clip;
     public AudioClip happiness_clip;
     public AudioClip sadness_clip;
     public AudioClip anger_clip;
@@ -26,21 +30,60 @@ public class DynamicMusic : MonoBehaviour
     private bool distortion_enable = false;
     private bool lowpass_enable = false;
 
-    // Emotion Detection Flags
-
-    private bool happiness_detected = false;
-    private bool sadness_detected = false;
-    private bool anger_detected = false;
-    private bool surprise_detected = false;
-
     // Other Variables
 
+    private int current_emotion = 0;            // Index of Current Detected Emotion (0: Default, 1: Happiness, 2: Sadness, 3: Anger, 4: Surprise)
+    private int detected_emotion = 0;           // Index of Newly Detected Emotion (0: Default, 1: Happiness, 2: Sadness, 3: Anger, 4: Surprise)
+    private int clip_time = 0;                  // Index of Precise Current Sample of Clip
+
     private GameObject player_object;
+
+    // ************************************************************************************
+    // Member Functions
+    // ************************************************************************************
+
+    // Change Clip on Emotion Change
+    private void changeClip(int emotion_index)
+    {
+        main_source.Stop();         // Stop Clip Playing
+
+        // Choose New Clip Based on Emotion
+        switch (emotion_index)
+        {
+            case 0:
+                main_source.clip = default_clip;
+                break;
+            case 1:
+                main_source.clip = happiness_clip;
+                break;
+            case 2:
+                main_source.clip = sadness_clip;
+                break;
+            case 3:
+                main_source.clip = anger_clip;
+                break;
+            case 4:
+                main_source.clip = surprise_clip;
+                break;
+        }
+
+        main_source.Play();         // Start Clip
+    }
+
+    // Detect Emotion Change
+    private void detectEmotionChange()
+    {
+        // Check that New Emotion is Different than Previous One
+        if (detected_emotion != current_emotion)
+        {
+            changeClip(detected_emotion);
+        }
+    }
 
     // Use this for initialization
     void Start()
     {
-        player_object = GameObject.FindWithTag("Player");
+        player_object = GameObject.FindWithTag("Player");       // Get Player GameObject
     }
 
     // Update is called once per frame
