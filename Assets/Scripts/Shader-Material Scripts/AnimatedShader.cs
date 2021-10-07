493c01a3c0eeb77ad11da4fcf86f54d9a8d871a4
pclank <31415965+pclank@users.noreply.github.com>
2021-10-07 19:24:28 +0300

    Added Custom Shaders, and Animated Shader Control Script.

---
+++ Assets/Scripts/Shader-Material Scripts/AnimatedShader.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedShader : MonoBehaviour
+{
+    // ************************************************************************************
+    // Public Variables
+    // ************************************************************************************
+
+    public float transition_speed = 0.2f;                       // Speed of Transition
+
+    // ************************************************************************************
+    // Private Variables
+    // ************************************************************************************
+
+    private bool forwards = true;                               // Whether Blending is Increasing
+
+    private Material mat;
+
+    // Use this for initialization
+    void Start()
+    {
+        mat =  gameObject.GetComponent<MeshRenderer>().material; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float next_state = gameObject.GetComponent<MeshRenderer>().material.GetFloat("BlendOpacity");
+
+        if (forwards && next_state < 1.0f)
+        {
+            next_state += transition_speed;
+
+            if (next_state >= 1.0f)
+            {
+                next_state = 1.0f;
+
+                forwards = false;
+            }
+        }
+
+        else if (!forwards && current_state > 0.0f)
+        {
+            next_state -= transition_speed;
+
+            if (next_state <= 0.0f)
+            {
+                next_state = 0.0f;
+
+                forwards = true;
+            }
+        }
+
+        gameObject.GetComponent<MeshRenderer>().material.SetFloat("BlendOpacity", next_state);
+    }
+}
