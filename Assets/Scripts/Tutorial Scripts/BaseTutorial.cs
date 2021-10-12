30609c64473ae172a3a5e435318db3aca346c4ba
pclank <31415965+pclank@users.noreply.github.com>
2021-10-12 19:29:26 +0300

    Completed Basic Structure of Tutorial Code.

--- Assets/Scripts/Tutorial Scripts/BaseTutorial.cs
+++ Assets/Scripts/Tutorial Scripts/BaseTutorial.cs
@@ -16,13 +16,21 @@ public class BaseTutorial : MonoBehaviour
     [Tooltip("Array of UI Elements to be Used by Tutorial.")]
     public GameObject[] ui_elements = new GameObject[4];
 
+    [Tooltip("Amount of Seconds to Run Each Tutorial.")]
+    public float[] delay = new float[4];
+
     // ************************************************************************************
     // Private Variables
     // ************************************************************************************
 
     private GameObject player_object;                                   // Player GameObject
 
+    private float timer_value = 0.0f;                                   // Timer Value
+
+    private bool tutorial_active = false;                               // Whether a Tutorial is Currently Running
     private bool first_tutorial_ran = false;                            // Whether First Tutorial Has Ran
+
+    private uint tutorial_index = 0;                                    // Index of Tutorial Running, Zero-Based
 
     // ************************************************************************************
     // Trigger Functions
@@ -34,7 +42,7 @@ public class BaseTutorial : MonoBehaviour
         {
             first_tutorial_ran = true;
 
-            startTutorial();
+            startTutorial(0);
         }
     }
 
@@ -42,12 +50,18 @@ public class BaseTutorial : MonoBehaviour
     // Member Functions
     // ************************************************************************************
     
-    // Start Base Tutorial
-    private void startTutorial()
+    // Start Tutorial
+    private void startTutorial(uint tut_index)
     {
-        freezePlayer();                         // Freeze Player Controller
+        tutorial_active = true;
 
+        tutorial_index = tut_index;
 
+        freezePlayer();                                     // Freeze Player Controller
+
+        ui_elements[tutorial_index].SetActive(true);        // Display UI Element
+
+        timer_value = Time.time;                            // Set Timer Value
     }
 
     // Freeze Player
@@ -73,6 +87,14 @@ public class BaseTutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Timer Section
 
+        if (tutorial_active && (Time.time - timer_value) >= delay[tutorial_index])
+        {
+            ui_elements[tutorial_index].SetActive(false);           // Disable UI Element
+            unfreezePlayer();                                       // Unfreeze Player
+
+            tutorial_active = false;
+        }
     }
 }
