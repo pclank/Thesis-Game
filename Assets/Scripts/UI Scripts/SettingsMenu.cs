018197ef19caa2858bc89680da9d79920940983f
pclank <31415965+pclank@users.noreply.github.com>
2021-10-21 19:09:44 +0300

    Added Initial Script for Settings Menu.

---
+++ Assets/Scripts/UI Scripts/SettingsMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+// ************************************************************************************
+// Settings Menu
+// ************************************************************************************
+
+public class SettingsMenu : MonoBehaviour
+{
+    // ************************************************************************************
+    // Public Variables
+    // ************************************************************************************
+
+    public GameObject menu_ui;
+
+    // ************************************************************************************
+    // Private Variables
+    // ************************************************************************************
+
+    private bool menu_enabled = false;                          // Whether Pause Menu is Enabled
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
