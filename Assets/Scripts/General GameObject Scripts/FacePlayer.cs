1c8e0d7642315ae49b0b772b72a0a8e9162e7ddf
pclank <31415965+pclank@users.noreply.github.com>
2021-11-01 21:18:52 +0200

    Finished Face Player Code.

--- Assets/Scripts/General GameObject Scripts/FacePlayer.cs
+++ Assets/Scripts/General GameObject Scripts/FacePlayer.cs
@@ -1,18 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// ************************************************************************************
+// Make GameObject Rotate to Face Player on Y-Axis
+// ************************************************************************************
+
 public class FacePlayer : MonoBehaviour
 {
+    // ************************************************************************************
+    // Public Variables
+    // ************************************************************************************
+
+    [Tooltip("Damping for Rotation.")]
+    public float damping = 1.0f;
+
+    // ************************************************************************************
+    // Private Variables
+    // ************************************************************************************
+
+    private GameObject player_object;
 
     // Use this for initialization
     void Start()
     {
-
+        player_object = GameObject.FindWithTag("Player");                   // Get Player GameObject
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 look_position = player_object.transform.position - transform.position;
+        look_position.y = 0;
 
+        Quaternion rotation = Quaternion.LookRotation(look_position);
+        rotation.eulerAngles = new Vector3(90.0f, rotation.eulerAngles.y, rotation.eulerAngles.z);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
     }
 }
