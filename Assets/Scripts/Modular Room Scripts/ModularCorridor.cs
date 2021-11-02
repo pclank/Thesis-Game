38795ede3d01687ab764f03efb8fb36f3bd9c1a9
pclank <31415965+pclank@users.noreply.github.com>
2021-11-02 20:08:02 +0200

    Added Initial Modular Corridor Control Script.

---
+++ Assets/Scripts/Modular Room Scripts/ModularCorridor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ************************************************************************************
+// Modular Corridor Control Script
+// ************************************************************************************
+
+public class ModularCorridor : MonoBehaviour
+{
+    // ************************************************************************************
+    // Public Variables
+    // ************************************************************************************
+
+    public GameObject door_wall_object;                         // Wall with Door GameObject
+    public GameObject end_wall_object;                          // Endwall GameObject
+
+    public int box_distance;                                    // Distance Denoting Player Boxes
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
