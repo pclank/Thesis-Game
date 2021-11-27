2f693eba321d23ead006c605c1bd0f5fb24b9b65
pclank <31415965+pclank@users.noreply.github.com>
2021-11-27 18:08:27 +0200

    Added Basic Structure of Wwise Dynamic Music Code.

---
+++ Assets/Scripts/Audio Scripts/WwiseDynamicMusic.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ************************************************************************************
+// Wwise Dynamic Music Control Script
+// ************************************************************************************
+
+public class WwiseDynamicMusic : MonoBehaviour
+{
+    // ************************************************************************************
+    // Public Variables
+    // ************************************************************************************
+
+    [Header("Wwise Settings - References")]
+    [Tooltip("Main Wwise Event.")]
+    public AK.Wwise.Event main_wwise_event;
+
+    [Header("Misc Options")]
+    [Tooltip("Track Blend Speed.")]
+    public float blend_speed = 0.5f;
+
+    [Tooltip("Delay Between Emotion Reading.")]
+    public float read_delay = 5.0f;
+
+    // ************************************************************************************
+    // Private Variables
+    // ************************************************************************************
+
+    private uint emotion_detected;
+
+    // ************************************************************************************
+    // Member Functions
+    // ************************************************************************************
+
+    // Read Emotion
+    private void readEmotion()
+    {
+        // TODO: Write Code!
+    }
+
+    // Enable Wwise Track
+    private void enableTrack()
+    {
+        // TODO: Write Code!
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
