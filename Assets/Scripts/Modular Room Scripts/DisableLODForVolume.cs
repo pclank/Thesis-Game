9455af5d6c3508f31089ae2a432fa2e3a32e334f
pclank <31415965+pclank@users.noreply.github.com>
2021-12-16 19:50:29 +0200

    Added Script to Disable LOD Upon Entering a Volume.

---
+++ Assets/Scripts/Modular Room Scripts/DisableLODForVolume.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ************************************************************************************
+// Script to Disable Target GameObjects Upon Entering this Volume
+// ************************************************************************************
+
+public class DisableLODForVolume : MonoBehaviour
+{
+    [Tooltip("GameObject to Disable.")]
+    public GameObject tgt_lod;
+
+    [Tooltip("Delay before Disabling.")]
+    public float delay = 2.0f;
+
+    private bool started = false;
+
+    private float timer_value = 0.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!started && other.CompareTag("Player"))
+        {
+            timer_value = Time.time;
+
+            started = true;
+        }
+    }
+
+    private void disableLOD()
+    {
+        tgt_lod.SetActive(false);
+
+        Destroy(this);
+    }
+
+    void Update()
+    {
+        if (started && Time.time - timer_value >= delay)
+            disableLOD();
+    }
+}
