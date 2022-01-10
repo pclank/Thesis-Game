bb1342ed1a251b49d8b53ef8839883e0f2f12a69
pclank <31415965+pclank@users.noreply.github.com>
2022-01-10 18:36:16 +0200

    Added Terms Agreement Functionality to Intro Setup Code.

--- Assets/Scripts/Modular Room Scripts/IntroSetup.cs
+++ Assets/Scripts/Modular Room Scripts/IntroSetup.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 using System.Collections;
 
@@ -15,10 +16,15 @@ public class IntroSetup : MonoBehaviour
     [Tooltip("Intro UI GameObject.")]
     public GameObject intro_ui;
 
+    public GameObject terms_ui;
+
     [Tooltip("Intro Background UI GameObject.")]
     public GameObject intro_background;
 
     public GameObject crosshair_ui;
+
+    public Button accept_button;
+    public Button refuse_button;
 
     public VideoPlayer video_player;
 
@@ -26,22 +32,46 @@ public class IntroSetup : MonoBehaviour
     // Private Variables
     // ************************************************************************************
 
+    private bool intro_over = false;
+
     // End of Clip Reached
     private void endReached(UnityEngine.Video.VideoPlayer vp)
     {
         vp.playbackSpeed = vp.playbackSpeed / 10.0F;
 
-        Time.timeScale = 1;
-
         video_player.Stop();
 
+        intro_over = true;
+
         Destroy(video_player.gameObject);
+
+        displayAgreement();
+    }
+
+    // Display Agreement UI
+    private void displayAgreement()
+    {
+        terms_ui.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;         // Unlock Cursor
+        Cursor.visible = true;                          // Make Cursor Visible
+    }
+
+    // Close Agreement UI
+    private void acceptAgreement()
+    {
+        terms_ui.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;       // Lock Cursor to Center
+        Cursor.visible = false;                         // Hide Cursor
 
         intro_ui.SetActive(false);
 
         crosshair_ui.SetActive(true);
 
         Destroy(intro_ui);
+
+        Time.timeScale = 1;                             // Unpause Game
 
         Destroy(this);
     }
@@ -55,11 +85,23 @@ public class IntroSetup : MonoBehaviour
         crosshair_ui.SetActive(false);
 
         video_player.loopPointReached += endReached;
+
+        accept_button.onClick.AddListener(() => buttonCallback(accept_button));
+        refuse_button.onClick.AddListener(() => buttonCallback(refuse_button));
     }
 
     void Update()
     {
-        if (video_player.isPlaying && video_player.clockTime >= 1.0f)
-            intro_ui.SetActive(false);
-    }    
+        if (!intro_over && video_player.isPlaying && video_player.clockTime >= 1.0f)
+            intro_background.SetActive(false);
+    }
+
+    // Button Callback
+    private void buttonCallback(Button pressed_button)
+    {
+        if (pressed_button == accept_button)
+            acceptAgreement();
+        else if (pressed_button == refuse_button)
+            Application.Quit();
+    }
 }
